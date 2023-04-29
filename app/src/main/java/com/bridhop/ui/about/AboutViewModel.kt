3815e66c81d622dcf7871e2235abe9eb26b58786
hotequil <hotequil@protonmail.com>
2023-04-29 10:22:47 -0300

    feat: replace Notifications and Dashboard to About and Products

--- app/src/main/java/com/bridhop/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/bridhop/ui/about/AboutViewModel.kt
@@ -1,13 +1,13 @@
-package com.bridhop.ui.dashboard
+package com.bridhop.ui.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is about Fragment"
     }
     val text: LiveData<String> = _text
 }

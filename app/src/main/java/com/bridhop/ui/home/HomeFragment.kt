30a0dd5a6c40315184217b02fcdb29bcee471d31
hotequil <hotequil@protonmail.com>
2023-04-29 13:17:50 -0300

    refactor: changed onCreateView methods

--- app/src/main/java/com/bridhop/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bridhop/ui/home/HomeFragment.kt
@@ -5,28 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bridhop.databinding.FragmentHomeBinding
+import com.bridhop.R
 
 class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
--- app/src/main/java/com/bridhop/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/bridhop/ui/products/ProductsFragment.kt
@@ -5,28 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bridhop.databinding.FragmentProductsBinding
+import com.bridhop.R
 
 class ProductsFragment : Fragment() {
-
-    private var _binding: FragmentProductsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentProductsBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_products, container, false)
     }
 }

ad82538ea4505fbf4eced3ffcc86b8fe447055b0
hotequil <hotequil@protonmail.com>
2023-04-29 13:32:37 -0300

    feat: used Intent to open FIAP site

--- app/src/main/java/com/bridhop/ui/about/AboutFragment.kt
+++ app/src/main/java/com/bridhop/ui/about/AboutFragment.kt
@@ -1,9 +1,12 @@
 package com.bridhop.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.bridhop.R
 
@@ -13,6 +16,17 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+        val moreAboutButton = view.findViewById<Button>(R.id.about_more_button)
+
+        moreAboutButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+
+            intent.setData(Uri.parse("https://fiap.com.br"))
+
+            startActivity(intent)
+        }
+
+        return view
     }
 }
